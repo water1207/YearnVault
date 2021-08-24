23d97b9409edbad85d6f3f5a56b83a7b2a99711b
Double D <doubled@DoubledeMac-mini.local>
2021-08-24 19:38:15 +0800

    update

--- contracts/YearnVault.sol
+++ contracts/YearnVault.sol
@@ -5,25 +5,39 @@ import "./ERC20.sol";
 
 contract YearnVault is ERC20 {
     ERC20 public underlyToken ;
+    address public addr;
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    constructor() ERC20("GameItem", "ITM", 100) public { 
-        underlyToken = ERC20(0x843b1940dCf1238c235eE38AB1462Ecb2A5c1e55);
+    constructor(address _addr) ERC20("vSCFX", "vSCFX", 10000) public { 
+        addr = _addr;
+        underlyToken = ERC20(addr);
+    }
+
+    function setAddr(address _addr) public {    //设置underlyToken的地址
+        addr = _addr;
+        underlyToken = ERC20(addr);
     }
 
     function deposit(uint256 amount, address recipient) external returns (uint256) {
         //require(underlyToken.balanceOf(msg.sender) > amount, "balance not enough");
-        underlyToken.transferFrom(recipient, address(this), amount);
-        return 0;
+        underlyToken.transferFrom(recipient, address(this), amount);    
+        uint256 shares = amount / 2;  //假设存100 underlyToken 得 50 LP
+        _mint(recipient, shares);
     }
 
-    // function withdraw(
-    //     uint256,
-    //     address,
-    //     uint256
-    // ) external returns (uint256);
+    function withdraw(
+        uint256 _amount,    //underlyToken 的 amount
+        address _destination,
+        uint256 _minUnderlying  //不知道干啥的
+    ) external returns (uint256 shares) {
+        shares = _amount / 2;
+        _burn(_destination, _amount);    //销毁LP
+        
+        underlyToken.transfer(_destination,  _amount); //返回underlyToken
+        return shares;    
+    }
 
     // // Returns the amount of underlying per each unit [1e18] of yearn shares
     // function pricePerShare() external view returns (uint256);
